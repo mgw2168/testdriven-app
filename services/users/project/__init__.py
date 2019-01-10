f2489e6c60a758e703500a0aff280833c005168c
xwp <xwp_fullstack@163.com>
2019-01-10 09:37:37 +0800

    模板创建

---
+++ services/users/project/__init__.py
@@ -0,0 +1,28 @@
+# services/users/project/__init__.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# instantiate the db
+db = SQLAlchemy()
+
+
+def create_app(script_info=None):
+
+    app = Flask(__name__)
+
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db.init_app(app)
+
+    # 注册蓝图
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
